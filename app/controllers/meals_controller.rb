c659135949ef8a518d4205f426ecf03cc1a8fb07
waytwelve <nvolle@gmail.com>
2017-11-29 13:23:56 +0100

    controller meals almost done

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -16,23 +16,43 @@ class MealsController < ApplicationController
   end
 
   def create
-    @meal = Meal.new
-
+    @meal = Meal.new(meal_params)
+    @meal.user = current_user
+    if @meal.save
+    flash[:notice] = "Meals #{@meal.name} has been created"
+    else
+    render :new
+    end
   end
 
   def edit
-
+    @meal = Meal.find(params[:id])
 
   end
 
   def update
+    @meal = Meal.find(params[:id])
+    @meal.update(meal_params)
+    redirect_to root_path
+    #@meal.user = current_user
 
   end
 
   def destroy
-    @meal = Meals.find(params[:id])
+    @meal = Meal.find(meal_params)
     @meal.destroy
+    redirect_to root_path
 
   end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :category, :country, :quantity, :is_delivery, :cooked_at, :user_id )
+  end
+
+
+
+
 
 end
