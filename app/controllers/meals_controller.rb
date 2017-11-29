8361c1accd02e7b67eec3f6606f679b43cc6f64a
Bouraoui Hassen <asn.code@gmail.com>
2017-11-29 14:41:34 +0100

    Ajout du front de show_meal V1

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,6 +1,8 @@
 class MealsController < ApplicationController
+ 
   before_action :set_meal, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show]
+  before_action :same_user, only: [:edit, :update]
 
   def index
     @meals = current_user.meals
@@ -16,19 +18,32 @@ class MealsController < ApplicationController
   def create
     @meal = current_user.meals.new(meal_params)
     if @meal.save
-      redirect_to meals_path, notice: 'Repas ajouté avec success !'
+      if params[:photos]
+        params[:images].each do |i|
+          @meal.photos.create(image: i)
+        end
+      end
+      @photos = @meal.photos
+      redirect_to edit_meal_path(@meal), notice: 'Repas ajouté avec success !'
     else
       render :new
     end
   end
 
   def edit
+    @photos = @meal.photos
 
   end
 
   def update
     if @meal.update(meal_params)
-      redirect_to meals_path, notice: 'Repas modifier avec success !'
+      if params[:photos]
+        params[:images].each do |i|
+          @meal.photos.create(image: i)
+        end
+      end
+      @photos = @meal.photos
+      redirect_to edit_meal_path(@meal), notice: 'Repas modifier avec success !'
     else
       render :edit
     end
@@ -46,6 +61,13 @@ class MealsController < ApplicationController
 
   def meal_params
     params.require(:meal).permit(:name, :category, :contry, :quantity, :is_delivery, :cooked_at, :description, :photo, :photo_cache)
+  end
+
+  def same_user
+    if current_user.id != @meal.user_id
+      flash[:danger] = "Vous ne pouver pas modifier cette page !"
+      redirect_to root_path
+    end
   end
 
 end
